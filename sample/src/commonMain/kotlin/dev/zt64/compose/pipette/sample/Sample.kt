0b0182926d5f517d61c5a252c7bf312233f60cce
zt64 <31907977+zt64@users.noreply.github.com>
2025-03-23 17:46:13 -0400

    feat(sample): improve ui layout to emphasize the picker components

--- sample/src/commonMain/kotlin/dev/zt64/compose/pipette/sample/Sample.kt
+++ sample/src/commonMain/kotlin/dev/zt64/compose/pipette/sample/Sample.kt
@@ -125,208 +125,159 @@ fun Sample() {
                 )
             }
         ) { paddingValues ->
-            @Composable
-            fun Sliders() {
-                Column(
-                    verticalArrangement = Arrangement.spacedBy(18.dp)
-                ) {
-                    Row(
-                        modifier = Modifier.height(IntrinsicSize.Min)
-                    ) {
-                        Column(
-                            modifier = Modifier.fillMaxHeight()
-                        ) {
-                            HexField(
-                                color = color,
-                                onColorChange = { newColor ->
-                                    hsvColor = HsvColor(newColor.hue, newColor.saturation, newColor.hsvValue)
-                                }
-                            )
-
-                            RgbField(
-                                color = color,
-                                onColorChange = { newColor ->
-                                    hsvColor = HsvColor(newColor.hue, newColor.saturation, newColor.hsvValue)
-                                }
-                            )
-
-                            HsvField(
-                                hsvColor = hsvColor,
-                                onColorChange = { newColor ->
-                                    hsvColor = HsvColor(newColor.hue, newColor.saturation, newColor.hsvValue)
-                                }
-                            )
-                        }
-
-                        Spacer(Modifier.width(12.dp))
-
-                        Box(
-                            modifier = Modifier
-                                .width(100.dp)
-                                .fillMaxHeight()
-                                .background(color, MaterialTheme.shapes.medium)
-                        )
-                    }
-
-                    Column {
-                        Text(
-                            text = "Hue",
-                            style = MaterialTheme.typography.titleMedium
-                        )
-
-                        Spacer(Modifier.height(8.dp))
-
-                        SampleSlider(
-                            value = hsvColor.hue,
-                            onValueChange = {
-                                hsvColor = hsvColor.copy(first = it)
-                            },
-                            valueRange = 0f..359f,
-                            brush = Brush.horizontalGradient(
-                                listOf(
-                                    Color.Red,
-                                    Color.Yellow,
-                                    Color.Green,
-                                    Color.Cyan,
-                                    Color.Blue,
-                                    Color.Magenta,
-                                    Color.Red
-                                )
-                            )
-                        )
-                    }
-
-                    Column {
-                        Text(
-                            text = "Saturation",
-                            style = MaterialTheme.typography.titleMedium
-                        )
-
-                        Spacer(Modifier.height(8.dp))
-
-                        SampleSlider(
-                            value = hsvColor.saturation,
-                            onValueChange = {
-                                hsvColor = hsvColor.copy(second = it)
-                            },
-                            valueRange = 0f..1f,
-                            brush = Brush.horizontalGradient(
-                                listOf(
-                                    Color.White,
-                                    Color.hsv(hsvColor.hue, 1f, hsvColor.value)
-                                )
-                            )
-                        )
-                    }
-
-                    Column {
-                        Text(
-                            text = "Value",
-                            style = MaterialTheme.typography.titleMedium
-                        )
-
-                        Spacer(Modifier.height(8.dp))
-
-                        SampleSlider(
-                            value = hsvColor.value,
-                            onValueChange = {
-                                hsvColor = hsvColor.copy(third = it)
-                            },
-                            valueRange = 0f..1f,
-                            brush = Brush.horizontalGradient(
-                                listOf(
-                                    Color.Black,
-                                    Color.hsv(hsvColor.hue, hsvColor.saturation, 1f)
-                                )
-                            )
-                        )
-                    }
-                }
-            }
-
-            @Composable
-            fun Pickers() {
-                FlowRow(
-                    modifier = Modifier.fillMaxSize(),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalArrangement = Arrangement.spacedBy(24.dp)
-                ) {
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text("Color Circle")
-
-                        Spacer(Modifier.height(6.dp))
-
-                        ColorCircle(
-                            color = color,
-                            onColorChange = { newColor ->
-                                hsvColor = hsvColor.copy(
-                                    first = newColor.hue,
-                                    second = newColor.saturation
-                                )
-                            }
-                        )
-                    }
-
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text("Color Square")
-
-                        Spacer(Modifier.height(6.dp))
-
-                        ColorSquare(
-                            hue = hsvColor.hue,
-                            saturation = hsvColor.saturation,
-                            value = hsvColor.value,
-                            onColorChange = { _, s, v ->
-                                hsvColor = hsvColor.copy(second = s, third = v)
-                            }
-                        )
-                    }
-
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text("Color Ring")
-
-                        Spacer(Modifier.height(6.dp))
-
-                        ColorRing(
-                            color = color,
-                            onColorChange = { newColor ->
-                                hsvColor = hsvColor.copy(first = newColor.hue)
-                            }
-                        )
-                    }
-                }
-            }
-
-            BoxWithConstraints(
+            Box(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(paddingValues)
-                    .padding(horizontal = 24.dp)
+                    .padding(horizontal = 24.dp),
+                contentAlignment = Alignment.Center
             ) {
-                when {
-                    maxWidth < 1000.dp -> {
-                        Column(
-                            modifier = Modifier
-                                .matchParentSize()
-                                .verticalScroll(rememberScrollState())
+                Column(
+                    modifier = Modifier.verticalScroll(rememberScrollState()),
+                    verticalArrangement = Arrangement.spacedBy(18.dp)
+                ) {
+                    FlowRow(
+                        horizontalArrangement = Arrangement.spacedBy(24.dp),
+                        verticalArrangement = Arrangement.spacedBy(24.dp)
+                    ) {
+                        Row(
+                            modifier = Modifier.height(IntrinsicSize.Min)
                         ) {
-                            Sliders()
-                            Pickers()
+                            Column(
+                                modifier = Modifier.fillMaxHeight()
+                            ) {
+                                HexField(
+                                    color = color,
+                                    onColorChange = { newColor ->
+                                        hsvColor = HsvColor(newColor.hue, newColor.saturation, newColor.hsvValue)
+                                    }
+                                )
+
+                                RgbField(
+                                    color = color,
+                                    onColorChange = { newColor ->
+                                        hsvColor = HsvColor(newColor.hue, newColor.saturation, newColor.hsvValue)
+                                    }
+                                )
+
+                                HsvField(
+                                    hsvColor = hsvColor,
+                                    onColorChange = { newColor ->
+                                        hsvColor = HsvColor(newColor.hue, newColor.saturation, newColor.hsvValue)
+                                    }
+                                )
+                            }
+
+                            Spacer(Modifier.width(12.dp))
+
+                            Box(
+                                modifier = Modifier
+                                    .width(100.dp)
+                                    .fillMaxHeight()
+                                    .background(color, MaterialTheme.shapes.medium)
+                            )
+                        }
+
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text("Color Circle")
+
+                            Spacer(Modifier.height(6.dp))
+
+                            ColorCircle(
+                                color = color,
+                                onColorChange = { newColor ->
+                                    hsvColor = hsvColor.copy(
+                                        first = newColor.hue,
+                                        second = newColor.saturation
+                                    )
+                                }
+                            )
+                        }
+
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text("Color Square")
+
+                            Spacer(Modifier.height(6.dp))
+
+                            ColorSquare(
+                                hue = hsvColor.hue,
+                                saturation = hsvColor.saturation,
+                                value = hsvColor.value,
+                                onColorChange = { _, s, v ->
+                                    hsvColor = hsvColor.copy(second = s, third = v)
+                                }
+                            )
+                        }
+
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text("Color Ring")
+
+                            Spacer(Modifier.height(6.dp))
+
+                            ColorRing(
+                                color = color,
+                                onColorChange = { newColor ->
+                                    hsvColor = hsvColor.copy(first = newColor.hue)
+                                }
+                            )
                         }
                     }
 
-                    else -> {
-                        Row {
-                            Sliders()
-                            Pickers()
-                        }
-                    }
+                    SampleSlider(
+                        value = hsvColor.hue,
+                        onValueChange = {
+                            hsvColor = hsvColor.copy(first = it)
+                        },
+                        valueRange = 0f..359f,
+                        text = "Hue",
+                        brush = Brush.horizontalGradient(
+                            listOf(
+                                Color.Red,
+                                Color.Yellow,
+                                Color.Green,
+                                Color.Cyan,
+                                Color.Blue,
+                                Color.Magenta,
+                                Color.Red
+                            )
+                        )
+                    )
+
+                    SampleSlider(
+                        value = hsvColor.saturation,
+                        onValueChange = {
+                            hsvColor = hsvColor.copy(second = it)
+                        },
+                        valueRange = 0f..1f,
+                        text = "Saturation",
+                        brush = Brush.horizontalGradient(
+                            listOf(
+                                Color.White,
+                                Color.hsv(hsvColor.hue, 1f, hsvColor.value)
+                            )
+                        )
+                    )
+
+                    SampleSlider(
+                        value = hsvColor.value,
+                        onValueChange = {
+                            hsvColor = hsvColor.copy(third = it)
+                        },
+                        valueRange = 0f..1f,
+                        text = "Value",
+                        brush = Brush.horizontalGradient(
+                            listOf(
+                                Color.Black,
+                                Color.hsv(hsvColor.hue, hsvColor.saturation, 1f)
+                            )
+                        )
+                    )
                 }
             }
         }
--- sample/src/commonMain/kotlin/dev/zt64/compose/pipette/sample/SampleSlider.kt
+++ sample/src/commonMain/kotlin/dev/zt64/compose/pipette/sample/SampleSlider.kt
@@ -6,9 +6,7 @@ import androidx.compose.foundation.hoverable
 import androidx.compose.foundation.interaction.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Slider
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -24,61 +22,71 @@ fun SampleSlider(
     value: Float,
     onValueChange: (Float) -> Unit,
     valueRange: ClosedFloatingPointRange<Float>,
+    text: String,
     brush: Brush
 ) {
-    val interactionSource = remember { MutableInteractionSource() }
+    Column {
+        val interactionSource = remember { MutableInteractionSource() }
 
-    Slider(
-        value = value,
-        onValueChange = onValueChange,
-        thumb = {
-            val interactions = remember { mutableStateListOf<Interaction>() }
+        Text(
+            text = "$text slider (demo only)",
+            style = MaterialTheme.typography.titleMedium
+        )
 
-            LaunchedEffect(interactionSource) {
-                interactionSource.interactions.collect { interaction ->
-                    when (interaction) {
-                        is PressInteraction.Press -> interactions.add(interaction)
-                        is PressInteraction.Release -> interactions.remove(interaction.press)
-                        is PressInteraction.Cancel -> interactions.remove(interaction.press)
-                        is DragInteraction.Start -> interactions.add(interaction)
-                        is DragInteraction.Stop -> interactions.remove(interaction.start)
-                        is DragInteraction.Cancel -> interactions.remove(interaction.start)
+        Spacer(Modifier.height(8.dp))
+
+        Slider(
+            value = value,
+            onValueChange = onValueChange,
+            thumb = {
+                val interactions = remember { mutableStateListOf<Interaction>() }
+
+                LaunchedEffect(interactionSource) {
+                    interactionSource.interactions.collect { interaction ->
+                        when (interaction) {
+                            is PressInteraction.Press -> interactions.add(interaction)
+                            is PressInteraction.Release -> interactions.remove(interaction.press)
+                            is PressInteraction.Cancel -> interactions.remove(interaction.press)
+                            is DragInteraction.Start -> interactions.add(interaction)
+                            is DragInteraction.Stop -> interactions.remove(interaction.start)
+                            is DragInteraction.Cancel -> interactions.remove(interaction.start)
+                        }
                     }
                 }
-            }
 
-            Box(
-                modifier = Modifier
-                    .size(28.dp)
-                    .hoverable(interactionSource = interactionSource)
-            ) {
-                val visualSize = if (interactions.isNotEmpty()) 28.dp else 24.dp
-
-                Spacer(
+                Box(
                     modifier = Modifier
-                        .size(visualSize)
-                        .align(Alignment.Center)
-                        .background(MaterialTheme.colorScheme.primary, CircleShape)
-                )
-            }
-        },
-        track = {
-            Canvas(
-                modifier = Modifier
-                    .widthIn(max = 700.dp)
-                    .height(12.dp)
-                    .fillMaxWidth()
-            ) {
-                drawLine(
-                    brush = brush,
-                    start = Offset(0f, size.center.y),
-                    end = Offset(size.width, size.center.y),
-                    strokeWidth = size.height,
-                    cap = StrokeCap.Round
-                )
-            }
-        },
-        valueRange = valueRange,
-        interactionSource = interactionSource
-    )
+                        .size(28.dp)
+                        .hoverable(interactionSource = interactionSource)
+                ) {
+                    val visualSize = if (interactions.isNotEmpty()) 28.dp else 24.dp
+
+                    Spacer(
+                        modifier = Modifier
+                            .size(visualSize)
+                            .align(Alignment.Center)
+                            .background(MaterialTheme.colorScheme.primary, CircleShape)
+                    )
+                }
+            },
+            track = {
+                Canvas(
+                    modifier = Modifier
+                        .widthIn(max = 700.dp)
+                        .height(12.dp)
+                        .fillMaxWidth()
+                ) {
+                    drawLine(
+                        brush = brush,
+                        start = Offset(0f, size.center.y),
+                        end = Offset(size.width, size.center.y),
+                        strokeWidth = size.height,
+                        cap = StrokeCap.Round
+                    )
+                }
+            },
+            valueRange = valueRange,
+            interactionSource = interactionSource
+        )
+    }
 }
